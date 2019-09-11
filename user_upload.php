d5ce9212e7717061b354f29a07147dda11dfc303
dlanaway <dlanaway@yahoo.com>
2019-09-11 16:29:45 +1000

    Add files via upload
    Main structure coding

--- user_upload.php
+++ user_upload.php
@@ -8,45 +8,64 @@ $arguments = getopt('u:p:h:', array('file:', 'create_table', 'dry_run', 'help'))
 
 // select the action based on what's in the arguments
 
-
 if (array_key_exists('help', $arguments)){ 
 	$msg = get_help();
 
 } else if (array_key_exists('create_table', $arguments)){ 
 	// check u/p/h are given
+	$uphGiven = checkUPH($arguments);
+	if (!$uphGiven[0]) {
+		$msg = $uphGiven[1];
+		
+	} else {
+		// create table if it doesn't exist
+		$username = $arguments['u'];
+		$password = $arguments['p'];
+		$host = $arguments['h'];
+		createUserTable($username, $password, $host);
 	
-	
-	// check if table exists
-	
-	
-	// create table
-	
-	
-	// success/failure msg
+		// success/failure msg
+		
+	}
 
 
 } else if (array_key_exists('file', $arguments)){ 
 	// check file exists first
+	$file = $arguments['file'];
+	$isFile = file_exists($file);
 	
-	// check file isn't empty
-	
-	
-	
-	
-
-	if (array_key_exists('dry_run', $arguments)){ 
-		// run through entries
+	if (!$isFile) {
+		// err msg
 		
-		// if none valid, err msg, else success msg
-
 	} else {
-		// check u/p/h are given
+		// check file isn't empty
+		if (filesize($file) == 0){
+			// err msg
+		} else {
 		
-		// run through entries
+			if (array_key_exists('dry_run', $arguments)){ 
+				$dryRun = true;
+				// run through entries
+				processFile($file, $dryRun);
+				
+				// if none valid, err msg, else success msg
+
+			} else {
+				// check u/p/h are given
+				$uphGiven = checkUPH($arguments);
+				if (!$uphGiven[0]) {
+					$msg = $uphGiven[1];
+				} else {
+					$dryRun = false;
+					// run through entries
+					processFile($file, $dryRun);
+					// if are valid entries insert into db, else no valid, err msg
 		
-		// if are valid entries insert into db, else no valid, err msg
+					// msg for insert db success/fail
 		
-		// msg for insert db success/fail
+				}
+			}
+		}
 		
 	}
 
@@ -56,6 +75,20 @@ if (array_key_exists('help', $arguments)){
 // output message
 
 fwrite(STDOUT, $msg);
+
+function checkUPH($arguments) {
+	
+}
+
+function createUserTable($username, $password, $host) {
+	
+}
+
+function processFile($file, $dryRun) {
+	// run through entries
+	
+}
+
 
 function get_help() {
 	
